792414e9910e7037b06dc8b7d3907a5c067284f3
DHindriks <dl.hindriks@gmail.com>
2024-05-18 17:30:29 +0200

    Layer Switching WIP

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Queue<Transform> WayPoints;
+    enum heightLayers
+    {
+        L1,
+        L2,
+        L3
+    };
+
     [SerializeField] Rigidbody rb;
     [SerializeField] int speed;
+
+    heightLayers Currentlayer;
+    bool SwitchingLayers = false;
+    float LayerSwitchTimeS = 1;
 
     void Start()
     {
@@ -17,5 +27,48 @@ public class PlayerMovement : MonoBehaviour
     {
         //transform.position += transform.forward * 10 * Time.deltaTime;
         rb.AddForce((transform.forward * speed) * Time.deltaTime);
+    }
+
+    void SwitchHeight(heightLayers NewLayer)
+    {
+        if (SwitchingLayers)
+        {
+            return;
+        }
+        Currentlayer = NewLayer;
+        IEnumerator Move;
+        switch (Currentlayer)
+        {
+            case heightLayers.L1:
+                Move = MoveLayers(15);
+                StartCoroutine(Move);
+                break;
+            case heightLayers.L2:
+                Move = MoveLayers(20);
+                StartCoroutine(Move); 
+                break;
+            case heightLayers.L3:
+                Move = MoveLayers(25);
+                StartCoroutine(Move);
+                break;
+        }
+    }
+
+    IEnumerator MoveLayers(float NewHeight)
+    {
+        SwitchingLayers = true;
+        float EndTime = Time.time + LayerSwitchTimeS;
+        float CurrentHeight = transform.position.y;
+        float CurrentProgress;
+        float StartTime = Time.time;
+        float CurrentY;
+        while (Time.time < EndTime)
+        {
+            CurrentProgress = Mathf.InverseLerp(StartTime, EndTime, Time.time);
+            CurrentY = Mathf.Lerp(CurrentHeight, NewHeight, CurrentProgress);
+            transform.position = new Vector3(transform.position.x, CurrentY, transform.position.z);
+        }
+
+        yield return null;
     }
 }
